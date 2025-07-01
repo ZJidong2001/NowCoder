d53f25b84ca47da4bf17fe007ad51c645f69c14f
Jidong-Zhang <94980508+ZJidong2001@users.noreply.github.com>
2025-07-01 21:28:57 +0800

    【模板】二维前缀和

---
+++ 226333 【模板】二维前缀和/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n = 0, m = 0, q = 0;
+    cin >> n >> m >> q;
+
+    vector<vector<int>> arr(n + 1, vector<int>(m + 1));
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+            cin >> arr[i][j];
+
+    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1));
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+            dp[i][j] = arr[i][j] - dp[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1];
+
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    while (q--)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
+    }
+
+    return 0;
+}
